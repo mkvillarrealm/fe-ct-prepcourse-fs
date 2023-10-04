25e344e06b0a47e48ac8a8cf8563d61b85a7a796
mkvillarrealm <mkvillarrealm@gmail.com>
2023-10-03 20:38:51 -0500

    Homework 7

---
+++ pruebas.js
@@ -0,0 +1,10 @@
+function Auto(puertas, color, marca, año, ruedas) {
+    this.puertas = puertas;
+    this.color = color;
+    this.marca = marca;
+    this.año = año;
+    this.ruedas = ruedas;
+ }
+ let miPrimerAuto = new Auto(2, 'Rojo', 'Ferrari', 2018, 4);
+ console.log(miPrimerAuto);
+ console.log(miPrimerAuto.marca);
